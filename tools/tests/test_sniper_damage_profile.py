714d2fb21a3e16a0abe0372747c8bbc8778d3c06
Blackrobe <14886923+Blackrobe@users.noreply.github.com>
2026-08-29 16:03:04 +0700

    Make Virus sniper payloads direct hit

--- tools/tests/test_sniper_damage_profile.py
+++ tools/tests/test_sniper_damage_profile.py
@@ -28,6 +28,7 @@ RESOLVED_SNIPERS = (
     "LightSniper", "CryoLightSniper", "RA2AWP",
     "NaxiSniper", "NaxiSniper_elite", "tkmawp", "VanSniper",
     "TSSniper", "td_gdi_commando_sniper", "td_gdi_commando_sniper_elite",
+    "RA2Virusgun", "RA2Virusgun2", "RA2Virusgun3", "RA2Virusgun_elite",
 )
 SPATIAL_DAMAGE_TYPES = {
     "AreaDamage", "AreaDamagePercentage", "OpenToppedDamage", "SpreadDamage",
